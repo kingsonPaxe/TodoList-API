445de073b2bc8af2a2b300d3ed44d17bb22bc462
KingsonPaxe <jeovanijp972@gmail.com.com>
2024-06-29 08:25:28 +0100

    Criei o meu arquivo index e o README

---
+++ README.txt
@@ -0,0 +1,31 @@
+************** Autor ***************
+Jeovani Paxe
+
+********** Descrição do projecto *********
+Tema: API to do List
+Descrição: Uma API para criar, listar, atualizar e excluir tarefas.
+
+Com essa API é possivel pode fazer o gerenciamento de tarefas onde o usuario possa visualizar os trabalhos
+ou as tarefas que ela pode realizar, os que ainda estao em falta e caso uma tarefa for concluida ele 
+podera excluir facilmente. 
+
+E para isso usei o framwork FastAPI para a criaçao desta mesma API.
+
+Operações CRUD:
+    * Create: Adicionar uma nova tarefa.
+    * Read: Listar todas as tarefas ou uma tarefa específica.
+    * Update: Atualizar o status ou os detalhes de uma tarefa.
+    * Delete: Excluir uma tarefa.
+
+*********** Libs usadas ***********
+FastAPI
+
+************* Instalação ***********
+pip install fastapi
+obs: Instalara todos os pacotes necessarios para o uso.
+
+************* Como executar ***********
+uvicor index:app --reload
+
+************** Para testar a API *********
+http://127.0.0.1:8000/docs
---
+++ index.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+class Task(BaseModel):
+    id: int
+    titulo: str
+    descricao: str = None
+    completado: bool = False
+
+tarefa = [
+    {
+        "id": 1,
+        "titulo": "Fazer a tarefa a prova de Progamação",
+        "descricao": "Fazer uma API em python para criar, listar, atualizar e excluir tarefas.",
+        "completado": True
+    }
+]
+
+def find_task(task_id: int):
+    for task in tarefa:
+        if task.id == task_id:
+            return task
+    return None
+
+# Create
+@app.post("/tarefa/", response_model=Task)
+def create_task(task: Task):
+    tarefa.append(task)
+    return task
+
+# Read
+@app.get("/tarefa/", response_model=List[Task])
+def get_tarefa():
+    return tarefa
+
+@app.get("/tarefa/{task_id}", response_model=Task)
+def get_task(task_id: int):
+    task = find_task(task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
+
+# Update
+@app.put("/tarefa/{task_id}", response_model=Task)
+def update_task(task_id: int, updated_task: Task):
+    task = find_task(task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    tarefa[tarefa.index(task)] = updated_task
+    return updated_task
+
+# Delete
+@app.delete("/tarefa/{task_id}", response_model=Task)
+def delete_task(task_id: int):
+    task = find_task(task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    tarefa.remove(task)
+    return task
